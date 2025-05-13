cda357f0aa16b920319763cc838eee78b79dfeb2
Gal Kremer <galkremer1@walla.co.il>
2025-05-13 12:14:25 -0400

    Fix config for production

--- next.config.js
+++ next.config.js
@@ -4,8 +4,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/personal-portfolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/personal-portfolio/' : '',
-}
+  basePath: process.env.NODE_ENV === 'production' ? '' : '',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
