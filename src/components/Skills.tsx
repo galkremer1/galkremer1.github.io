89659b49f359a30ce14650ecef242005705fe59a
Gal Kremer <galkremer1@walla.co.il>
2025-05-14 16:25:04 -0400

    Fix skills styling - align items to center

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,7 +1,5 @@
 import React, { FC } from 'react';
-
 import { SkillIcon, SkillIconProps } from 'react-skills-icons';
-
 import { IconType } from 'react-icons';
 
 const skills: (
@@ -41,15 +39,16 @@ const Skills: React.FC = () => {
   return (
     <section id="skills">
       <h1 className="text-4xl font-bold text-center mb-6">Skills</h1>
-      <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 align-items-center justify-items-center mt-8">
+      <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 justify-items-center mt-8">
         {skills.map((skill) => (
-          <SkillIcon
-            key={skill.name}
-            customIcon={skill.icon}
-            skill={skill.name as SkillIconProps['skill']}
-            className="hover:shadow-xl/20 w-34 rounded-xl !block align-items-center justify-items-center hover:scale-120 transition-transform duration-400 cursor-pointer"
-            size={34}
-          />
+          <div key={skill.name} className="flex flex-col items-center w-full">
+            <SkillIcon
+              customIcon={skill.icon}
+              skill={skill.name as SkillIconProps['skill']}
+              className="!flex !flex-col !items-center w-34 hover:shadow-xl/20 rounded-xl hover:scale-115 transition-transform duration-300 cursor-pointer"
+              size={34}
+            />
+          </div>
         ))}
       </div>
     </section>
