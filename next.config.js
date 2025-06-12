58d9523e64eaedbd1144bef157b13c2b5452d5c6
Gal Kremer <galkremer1@walla.co.il>
2025-06-11 22:15:44 -0400

    Basepath update

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? 'galkremer1.github.io' : '',
+  basePath:
+    process.env.NODE_ENV === 'production' ? '/galkremer1.github.io' : '',
 };
 
 module.exports = nextConfig;
